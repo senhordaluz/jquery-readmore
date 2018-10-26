b824ec5fc0441ba5975d65018992df6554752618
Pedro da Luz <pedro.luz@hotmail.com.br>
2018-10-26 18:04:35 -0300

    Ver 1.7 - hide function added

--- readmore/jquery.readmore.js
+++ readmore/jquery.readmore.js
@@ -1,24 +1,31 @@
 /**
  * jquery.readmore
  * 
+ * Based on jquery.readmore plugin by Jake Trent & Mike Wendt
+ * 
  * Substring long paragraphs and make expandable with "Read More" link.
  * Paragraph will be split either at exactly 'substr_len' chars or at the next
  * space char after 'substr_len' words (default).
  * 
- * @date 02 Jul 2012
+ * @date 26 Out 2018
  * @author Jake Trent (original author) http://www.builtbyjake.com
  * @author Mike Wendt http://www.mikewendt.net
- * @version 1.6
+ * @author Pedro da Luz https://github.com/senhordaluz
+ * @version 1.7
  */
 (function ($) {
   $.fn.readmore = function (settings) {
 
     var defaults = {
       substr_len: 500,
+      speed: 500,
       split_word: false,
       ellipses: '&#8230;',
-      more_link: '<a class="readm-more">Read&nbsp;More</a>',
+      more_link: '<a class="readm-more">Read&nbsp;more</a>',
       more_clzz: 'readm-more',
+      show_hide: true,
+      less_link: '<a class="readm-less">Hide</a>',
+      less_clzz: 'readm-less',
       ellipse_clzz: 'readm-continue',
       hidden_clzz: 'readm-hidden'
     };
@@ -38,8 +45,19 @@
       elem.find('.' + opts.more_clzz).click( function () {
         $(this).hide();
         elem.find('.' + opts.ellipse_clzz).hide();
-        elem.find('.' + opts.hidden_clzz).animate({'opacity' : 'toggle'},1000);
+        elem.find('.' + opts.hidden_clzz).animate({'opacity' : 'toggle'},opts.speed,'swing',function() {
+          if (opts.show_hide) elem.find('.' + opts.less_clzz).show();
+        });
       });
+      if (opts.show_hide) {
+        elem.find('.' + opts.less_clzz).click( function () {
+          $(this).hide();
+          elem.find('.' + opts.hidden_clzz).animate({'opacity' : 'toggle'},opts.speed,'swing',function() {
+            elem.find('.' + opts.ellipse_clzz).show();
+            elem.find('.' + opts.more_clzz).show();
+          });
+        });
+      }
     }
 
     function abridge(elem) {
@@ -49,6 +67,7 @@
       var hidden =
         '<span class="' + opts.hidden_clzz + '" style="display:none;">' +
           txt.substring((opts.split_word ? opts.substr_len : txt.indexOf(' ', opts.substr_len)), txt.length) +
+          (opts.show_hide ? opts.less_link : '') +
         '</span>';
       elem.html(shown + hidden);
     }
